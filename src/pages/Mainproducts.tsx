45bbcbe8a869e2beb6107a6fa96a8605d27227a0
Shriprasanna B <shriprasanna32@gmail.com>
2022-10-09 19:18:08 +0530

    png added

--- src/pages/Mainproducts.tsx
+++ src/pages/Mainproducts.tsx
@@ -6,6 +6,7 @@ export interface ProductsProps {
   img: any;
   _id: number;
 }
+// images\three-s.png
 
 export const products: ProductsProps[] = [
   {
@@ -17,31 +18,31 @@ export const products: ProductsProps[] = [
   {
     name: "Tshirt (shriBlue)",
     price: 150,
-    img: "./images/two-s",
+    img: "./images/two-s.png",
     _id: 2,
   },
   {
     name: "Tshirt (Blue)",
     price: 100,
-    img: "./images/three-s",
+    img: "./images/three-s.png",
     _id: 3,
   },
   {
     name: "Pant (black)",
     price: 100,
-    img: "./images/one-p",
+    img: "./images/one-p.png",
     _id: 4,
   },
   {
     name: "Pant (shriBlue)",
     price: 150,
-    img: "./images/two-p",
+    img: "./images/two-p.png",
     _id: 5,
   },
   {
     name: "Pant (Blue)",
     price: 100,
-    img: "./images/three-p",
+    img: "./images/three-p.png",
     _id: 6,
   },
 ];
