d4379322f8ab7fb7faad8232ae93589432f30ece
Shriprasanna B <shriprasanna32@gmail.com>
2022-10-11 20:42:55 +0530

    ui done / pant img change / main code form tmr

--- src/pages/Mainproducts.tsx
+++ src/pages/Mainproducts.tsx
@@ -1,28 +1,46 @@
 import t1 from "./images/one-s.png";
 import t2 from "./images/two-s.png";
 import t3 from "./images/three-s.png";
-// import p1 from "./images/one-p.png";
-// import p2 from "./images/two-p.png";
-// import p3 from "./images/three-p.png";
+import p1 from "./images/one-p.png";
+import p2 from "./images/two-p.png";
+import p3 from "./images/three-p.png";
+import s1 from "./images/sock1.png";
+import s2 from "./images/sock2.png";
+import s3 from "./images/sock3.png";
 
-export interface ProductsProps {
+export interface Products {
   name: string;
   price: number;
   img: string[];
   _id: number;
 }
 
-export const products: ProductsProps[] = [
+export const ShirtList: Products[] = [
   {
     name: "Tshirt",
     price: 100,
     img: [t1, t2, t3],
     _id: 1,
   },
-  // {
-  //   name: "Pant",
-  //   price: 100,
-  //   img: [p1, p2, p3],
-  //   _id: 4,
-  // },
+  //need to add more
+];
+
+export const SocksList: Products[] = [
+  {
+    name: "Socks",
+    price: 100,
+    img: [s1, s2, s3],
+    _id: 1,
+  },
+  //need to add more
+];
+
+export const PantsList: Products[] = [
+  {
+    name: "Pant",
+    price: 100,
+    img: [p1, p2, p3],
+    _id: 1,
+  },
+  //need to add more
 ];
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from "react";
-import { products } from "./Mainproducts";
+import { ShirtList } from "./Mainproducts";
+import { BsFillCircleFill } from "react-icons/bs";
 
 const Products: FC = () => {
   const [shirt, setShirt] = useState<number>(0);
@@ -8,7 +9,7 @@ const Products: FC = () => {
 
   return (
     <div>
-      {products.map((i, k) => {
+      {ShirtList.map((i, k) => {
         return (
           <main className="flex justify-center items-center" key={k}>
             <div
@@ -16,7 +17,7 @@ const Products: FC = () => {
              border-black p-2 w-[270px] rounded-md bg-sky-300"
             >
               <div
-                className="bg-[#E7DBDB] w-[250px] h-[200px] flex justify-center 
+                className="bg-[#E7DBDB] w-[250px] h-[220px] flex justify-center 
             items-center rounded-md"
               >
                 <img src={i.img[shirt]} alt={i.name} className="w-[230px]" />
@@ -27,13 +28,29 @@ const Products: FC = () => {
                 <h1 className="text-2xl font-bold underline">${i.price}</h1>
               </div>
 
-              <div className="flex gap-7">
-                <button onClick={() => setShirt(0)}>black</button>
-                <button onClick={() => setShirt(1)}>shriBlue</button>
-                <button onClick={() => setShirt(2)}>Blue</button>
-              </div>
+              <div className="flex justify-around w-[300px] mt-3">
+                <div className="flex justify-between w-[80px]">
+                  <button onClick={() => setShirt(0)}>
+                    <BsFillCircleFill className="text-2xl text-[#393939]" />
+                  </button>
 
-              <button>Add to cart</button>
+                  <button onClick={() => setShirt(1)}>
+                    <BsFillCircleFill className="text-2xl text-[#3c8bad]" />
+                  </button>
+
+                  <button onClick={() => setShirt(2)}>
+                    <BsFillCircleFill className="text-2xl text-[#365f9d]" />
+                  </button>
+                </div>
+
+                <button
+                  className="w-[100px] border-2 border-black rounded-md h-[35px]
+                font-bold bg-black text-sky-300 hover:bg-white hover:text-black
+                transition-all duration-150 hover:w-[102px] text-[14px]"
+                >
+                  Add to cart
+                </button>
+              </div>
             </div>
           </main>
         );
